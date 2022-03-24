db1d3054a1369a7479e9f56cdfab9aefe2b61b78
thmhugo <hugo.thomas37300@gmail.com>
2022-03-24 08:55:27 +0100

    feat: wrote the lp

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-import itertools
-from pprint import pprint
+from itertools import product
+import gurobipy as gp
 
 
 def vec_d_lambda(l):
     dl = []
-    permutations = list(itertools.product([0, 1], repeat=2))
+    permutations = list(product([0, 1], repeat=2))
 
     for (x, y) in permutations:
         for (a, b) in permutations:
@@ -13,15 +13,44 @@ def vec_d_lambda(l):
     return dl
 
 
-def vec_p():
-    return [
-        int((a + b) % 2 == x * y)
-        for a, b, x, y in list(itertools.product([0, 1], repeat=4))
-    ]
+lambdas = list(product([0, 1], repeat=4))
+
+# for l in lambdas:
+#     print(f"{l} -> {vec_d_lambda(l)}")
+
+dot = lambda A, B: gp.quicksum(A[i] * B[i] for i in range(N))
+
+delta = 2
+m = 2
+
+N = delta ** (2 * m)
+p = [int((a + b) % 2 == x * y) for a, b, x, y in list(product([0, 1], repeat=4))]
+
+# Create a new model
+m = gp.Model()
 
 
-lambdas = list(itertools.product([0, 1], repeat=4))
+# Create variables
+S = [m.addVar(name=f"s_{i}", vtype="C") for i in range(N)]
+S_l = m.addVar(name="S_l", vtype="C")
+m.update()
+
+
+# Set objective function
+m.setObjective(dot(S, p) - S_l, gp.GRB.MAXIMIZE)
+
+
+# Add constraints
 for l in lambdas:
-    print(f"{l} -> {vec_d_lambda(l)}")
+    m.addConstr(dot(S, vec_d_lambda(l)) - S_l <= 0)
 
-print(vec_p())
+m.addConstr(dot(S, p) - S_l <= 1)
+
+
+# Solve it!
+m.optimize()
+
+print(f"Optimal objective value = {m.objVal}")
+print(f"Solution values:    S_l = {S_l.X}")
+print(f"                      S = {[S[i].X for i in range(N)]}")
+print(f"             (recall) p = {p}")
