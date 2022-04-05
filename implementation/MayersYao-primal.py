ac31f85d99dd90fa4602d65b59877a74ea55c28f
fterrier15 <fterrier15@protonmail.com>
2022-04-05 12:30:30 +0200

    primal et dual update

--- implementation/MayersYao-primal.py
+++ implementation/MayersYao-primal.py
@@ -1,87 +1,86 @@
 from math import sqrt
-import numpy as np
 from itertools import product
 import collections
 from gurobipy import *
+import numpy as np
+
+np.set_printoptions(threshold=np.inf)
 
 # inputs (x,y) domain and outputs (a,b) domain
-domain_xy = [0, 1]
+domain_xy = [0, 1, 2]
 domain_ab = [-1, 1]
+delta = len(domain_ab)
+m = len(domain_xy)
 
-# dictionnary giving the basis order, i.e p[(a,b,x,y)] = index of p(ab|xy) in the basis order chosen
-p = collections.defaultdict(int)
+# Cardinal of the basis of proba P(ab|xy)
+N = (delta * m) ** 2
+
+ # Stores the index of each P(a,b,x,y)
+indexes_p = collections.defaultdict(int)
 i = 0
-for x, y in product(domain_xy, repeat=2):
-    for a, b in product(domain_ab, repeat=2):
-        p[a, b, x, y] = i
+for a, b in product(domain_ab, repeat=2):
+    for x, y in product(domain_xy, repeat=2):
+        indexes_p[a, b, x, y] = i
         i += 1
-
-print("\n ------  basis order\n")
-print(p)
-print("\n\n")
 
 #-------------------------------------------------------------------------------
 #       QUANTUM CORR
 #-------------------------------------------------------------------------------
 
-def quantum_corr():
-        """
-        Returns the probability distribution according to the CHSH
-        correlations.
-        """
-    # <ψ| A_i x B_j |ψ> = 1/sqrt(2) * ij
-    A1 = np.zeros([4, 16])
-    B1 = np.zeros([4])
-    i = 0
-    for x, y in product(domain_xy, repeat=2):
-        for a, b in product(domain_ab, repeat=2):
-            A1[i, p[a, b, x, y]] = a * b
-        B1[i] = (-1) ** (x * y) / sqrt(2)
-        i += 1
-
-    # <ψ| A_i x B_j |ψ> = 1
-    A2 = np.zeros([4, 16])
-    B2 = np.zeros([4])
-    i = 0
-    for x, y in product(domain_xy, repeat=2):
-        for a, b in product(domain_ab, repeat=2):
-            A2[i, p[a, b, x, y]] = 1
-        B2[i] = 1
-        i += 1
-
-    # <ψ| A_i x 1_B |ψ> = 0
-    A3 = np.zeros([4, 16])
-    B3 = np.zeros([4])
-    i = 0
-    for x, y in product(domain_xy, repeat=2):
-        for a, b in product(domain_ab, repeat=2):
-            A3[i, p[a, b, x, y]] = a
-        B3[i] = 0
-        i += 1
-    print(A3)
-    # <ψ| 1_A x B_j |ψ> = 0
-    A4 = np.zeros([4, 16])
-    B4 = np.zeros([4])
-    i = 0
-    for x, y in product(domain_xy, repeat=2):
-        for a, b in product(domain_ab, repeat=2):
-            A4[i, p[a, b, x, y]] = b
-        B4[i] = 0
-        i += 1
-
+def ls_quantum_p():
+    """
+    Returns the probability distribution according to the Mayers-Yao
+    correlations.
+    """
     # Constraint Coefficients
-    A = np.concatenate((A1, A2, A3, A4), axis=0)
+    A = np.zeros([N, N])
+    # Left Side
+    B = np.zeros([N])
+    i = 0
+    print(f"{N = }")
 
-    # Values
-    B = np.concatenate((B1, B2, B3, B4), axis=None)
+    for z in domain_xy:
+        for a, b in product(domain_ab, repeat=2):
+            A[i, indexes_p[a, b, z, z]] = a * b
+        B[i] = 1
+        i += 1
 
-    # Solve linear system AP = B to have the vector of quantum correlations
-    P = np.linalg.solve(A, B)
+    for x in [0, 1]:
+        y = 1 - x
+        for a, b in product(domain_ab, repeat=2):
+            A[i, indexes_p[a, b, x, y]] = a * b
+        B[i] = 0
+        i += 1
 
+    for x, y in [(0, 2), (1, 2), (2, 0), (2, 1)]:
+        for a, b in product(domain_ab, repeat=2):
+            A[i, indexes_p[a, b, x, y]] = a * b
+        B[i] = 1 / sqrt(2)
+        i += 1
+
+    for x, y in product(domain_xy, repeat=2):
+        for a, b in product(domain_ab, repeat=2):
+            A[i, indexes_p[a, b, x, y]] = 1
+        B[i] = 1
+        i += 1
+
+    for x, y in product(domain_xy, repeat=2):
+        for a, b in product(domain_ab, repeat=2):
+            A[i, indexes_p[a, b, x, y]] = a
+        B[i] = 0
+        i += 1
+
+    for x, y in product(domain_xy, repeat=2):
+        for a, b in product(domain_ab, repeat=2):
+            A[i, indexes_p[a, b, x, y]] = b
+        B[i] = 0
+        i += 1
+
+    # Solve linear system Ax = B
+    P = list(np.linalg.solve(A, B))
     print("\n ------  Quantum Correlations (CHSH)")
     print(P) ;
     print("\n\n")
-
     return P
 
 #-------------------------------------------------------------------------------
@@ -92,7 +91,7 @@ def local_corr():
     P = np.zeros(len(domain_ab)**2*len(domain_xy)**2)
     for x, y in product(domain_xy, repeat=2):
         for a, b in product(domain_ab, repeat=2):
-            index = p[(a,b,x,y)]
+            index = indexes_p[(a,b,x,y)]
             P[index]= 1/4.
 
     print("\n ------  Local Correlations")
@@ -105,31 +104,38 @@ def local_corr():
 #       DETRMINISTIC BEHAVIOR
 #-------------------------------------------------------------------------------
 
-def vec_d_lambda(l):
+def vec_d_lambda(l: int):
+    """Generates the D_lambda vector associated to a lambda.
+    Args:
+        l (list[int]): a behavior lambda
+    Returns:
+        list[int]: the vector D_lambda
+    """
     dl = []
-    sum = 0
-    i=0
-    for x, y in product(domain_xy, repeat=2):
-        for a, b in product(domain_ab, repeat=2):
+    for x, y in list(product(domain_xy, repeat=2)):
+        for a, b in list(product(domain_ab, repeat=2)):
             dl.append(int(l[x] == a and l[y + 2] == b))
     return dl
 
-# Lambdas are the possible outputs assignement (a0,a1,b0,b1), there are 16 possible lambdas
+# Lambdas are the possible outputs assignement (a0,a1,a2,b0,b1,b2), there are 16 possible lambdas
+
 lambdas = []
-for a0, a1 in product(domain_ab, repeat=2):
-    for b0, b1 in product(domain_ab, repeat=2):
-        lambdas.append((a0, a1, b0, b1))
+for a0, a1, a2 in list(product([-1, 1], repeat=3)):
+    for b0, b1, b2 in list(product([-1, 1], repeat=3)):
+        lambdas.append((a0, a1, a2, b0, b1, b2))
+
 
 #D_l is a matrix with each row corresponding to a deterministic behavior lambda
-D_l = np.zeros((len(lambdas),len(lambdas)))
+D_l = np.zeros((len(lambdas),N)) # vector 64*36
 i=0
 for l in lambdas :
     D_l[i] = np.array(vec_d_lambda(l))
     i+=1
-
 # M is the transpose of D_l , one column = one deterministic behavior d_lambda
 M = np.column_stack(D_l)
 
+
+P = ls_quantum_p()
 
 #-------------------------------------------------------------------------------
 #       LINEAR PROGRAMMING WITH GUROBI SOLVER
@@ -144,10 +150,11 @@ k = int(input())
 if k==0 :
     P = local_corr()
 if k==1 :
-    P = quantum_corr()
+    P = ls_quantum_p()
 
 # mu_lambda is a vector of the coeff of the linear combination of the vectors d_lambda
 mu_lambda = [m.addVar(name=f"mu_{i}", vtype="C") for i in range(len(lambdas))]
+
 
 # P_l : vector  of the convex combination of the deterministic points,
 # i.e P_l = sum(mu_lambda * vec_d_lambda) where the sum is on the lambdas
@@ -156,13 +163,11 @@ P_l = np.dot(M,mu_lambda)
 #add a variable Q (visibility)
 Q = m.addVar(name="Q", vtype="C")
 
-
-#update the model with the newly defined variables
 m.update()
 
 # Add the constraints
 for i in range(len(P)):
-    m.addConstr((Q*P[i] >= P_l[i])  )
+    m.addConstr((Q*P[i] >= P_l[i]) )
 
 m.addConstr(quicksum(mu_lambda[i] for i in range(len(lambdas))) == 1)
 
@@ -170,6 +175,7 @@ for i in range(len(lambdas)):
     m.addConstr(mu_lambda[i] >= 0)
 
 m.update()
+
 
 # objective = Min Q
 m.setObjective(Q, GRB.MINIMIZE)
