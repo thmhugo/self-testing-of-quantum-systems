a5b14bc9f1460682ab65e5c96ce0a396ab646801
thmhugo <hugo.thomas37300@gmail.com>
2022-04-30 16:47:40 +0200

    feat: non linear lp using game object

--- implementation/chsh-primal-nl.py
+++ implementation/chsh-primal-nl.py
@@ -4,134 +4,112 @@ from itertools import product
 import collections
 from gurobipy import *
 from decimal import Context
-
-# inputs (x,y) domain and outputs (a,b) domain
-domain_xy = [0, 1]
-domain_ab = [-1, 1]
-
-# dictionnary giving the basis order, i.e p[(a,b,x,y)] = index of p(ab|xy) in the basis order chosen
-p = collections.defaultdict(int)
-i = 0
-for x, y in product(domain_xy, repeat=2):
-    for a, b in product(domain_ab, repeat=2):
-        p[a, b, x, y] = i
-        i += 1
+from gurobipy import *
+from game import *
 
 
-def vec_d_lambda(l):
-    dl = []
-    for x, y in product(domain_xy, repeat=2):
-        for a, b in product(domain_ab, repeat=2):
-            dl.append(int(l[x] == a and l[y + 2] == b))
-    return dl
-
-
-# Lambdas are the possible outputs assignement (a0,a1,b0,b1), there are 16 possible lambdas
-lambdas = []
-for a0, a1 in product(domain_ab, repeat=2):
-    for b0, b1 in product(domain_ab, repeat=2):
-        lambdas.append((a0, a1, b0, b1))
-
-# D_l is a matrix with each row corresponding to a deterministic behavior lambda
-D_l = [np.array(vec_d_lambda(l)) for l in lambdas]
-
-# M is the transpose of D_l , one column = one deterministic behavior d_lambda
-M = np.column_stack(D_l)
-
-
-random = [0.25] * 16
-
-
-def get_model():
-    m = Model()
-    m.Params.LogToConsole = 0
-    m.params.NonConvex = 2  # Needed since the model is non convex.
+def get_model(game):
+    game = Game(domain_ab=game.domain_ab, domain_xy=game.domain_xy)
+    game.model.Params.LogToConsole = 0
+    game.model.params.NonConvex = 2  # Needed since the model is non convex.
 
     # mu_lambda is a vector of the coeff of the linear combination of the vectors d_lambda
-    mu_lambda = [m.addVar(name=f"mu_{i}", vtype="C") for i in range(len(lambdas))]
+    mu_lambda = [
+        game.model.addVar(name=f"mu_{i}", vtype="C") for i in range(len(game.lambdas))
+    ]
 
-    P = [m.addVar(name=f"p_{i}", vtype="C") for i in range(16)]
+    P = [game.model.addVar(name=f"p_{i}", vtype="C") for i in range(16)]
     # P_l : vector  of the convex combination of the deterministic points,
     # i.e P_l = sum(mu_lambda * vec_d_lambda) where the sum is on the lambdas
-    P_l = np.dot(M, mu_lambda)
+    P_l = np.dot(game.M, mu_lambda)
 
     # add a variable Q (visibility)
-    Q = m.addVar(name="Q", vtype="C")
+    Q = game.model.addVar(name="Q", vtype="C")
 
     # update the model with the newly defined variables
-    m.update()
+    game.model.update()
 
     # Add the constraints
     for i in range(len(P)):
-        m.addConstr(((1 - Q) * P[i] + Q - P_l[i] == 0))
+        game.model.addConstr(((1 - Q) * P[i] + Q - P_l[i] == 0))
 
     # Constraints on probabilities
-    m.addConstr(quicksum(mu_lambda[i] for i in range(len(lambdas))) >= 1)
+    game.model.addConstr(quicksum(mu_lambda[i] for i in range(len(game.lambdas))) >= 1)
 
-    for i in range(len(lambdas)):
-        m.addConstr(mu_lambda[i] >= 0)
-        m.addConstr(P[i] >= 0)
+    for i in range(len(game.lambdas)):
+        game.model.addConstr(mu_lambda[i] >= 0)
+        game.model.addConstr(P[i] >= 0)
 
     # Define a constraint for the chsh inequality
     chsh = LinExpr()
-    for x, y in product(domain_xy, repeat=2):
+    for x, y in product(game.domain_xy, repeat=2):
         e = LinExpr()  # represents an expecation value <Ax By>
-        for a, b in product(domain_ab, repeat=2):
-            e += a * b * P[p[a, b, x, y]]
+        for a, b in product(game.domain_ab, repeat=2):
+            e += a * b * P[game.indexes_p[a, b, x, y]]
         # Constraints from (10) p.6 : Bell nonlocality
-        m.addConstr(e <= 1)
-        m.addConstr(e >= -1)
+        game.model.addConstr(e <= 1)
+        game.model.addConstr(e >= -1)
         chsh += (-1) ** (x * y) * e
 
-    for x, y in product(domain_xy, repeat=2):
+    for x, y in product(game.domain_xy, repeat=2):
         # Each marginal must sum up to one
-        m.addConstr(
-            quicksum(P[p[a, b, x, y]] for a, b in product(domain_ab, repeat=2)) == 1
+        game.model.addConstr(
+            quicksum(
+                P[game.indexes_p[a, b, x, y]]
+                for a, b in product(game.domain_ab, repeat=2)
+            )
+            == 1
         )
     # If useless, removed by guroby
     # m.addConstr(quicksum(P[i] for i in range(len(lambdas))) == 4)
 
-    for x, y in product(domain_xy, repeat=2):
-        m.addConstr(
-            quicksum(a * P[p[a, b, x, y]] for a, b in product(domain_ab, repeat=2)) == 0
+    for x, y in product(game.domain_xy, repeat=2):
+        game.model.addConstr(
+            quicksum(
+                a * P[game.indexes_p[a, b, x, y]]
+                for a, b in product(game.domain_ab, repeat=2)
+            )
+            == 0
         )
 
-    for x, y in product(domain_xy, repeat=2):
-        m.addConstr(
-            quicksum(b * P[p[a, b, x, y]] for a, b in product(domain_ab, repeat=2)) == 0
+    for x, y in product(game.domain_xy, repeat=2):
+        game.model.addConstr(
+            quicksum(
+                b * P[game.indexes_p[a, b, x, y]]
+                for a, b in product(game.domain_ab, repeat=2)
+            )
+            == 0
         )
 
-    m.addConstr(Q >= 0)
-    m.addConstr(Q <= 1)
-    return m, chsh, P, Q
+    game.model.addConstr(Q >= 0)
+    game.model.addConstr(Q <= 1)
+    return game, chsh, P, Q
 
 
-def find_optimal_p():
+def find_optimal_p(game):
     run = True
-    d = 10e-5
+    d = 10e-4
     upper_bound = 0
 
     c = Context(prec=15)
 
-    # m.remove(bound)
-    print("\n\n")
     while run:
-        m, chsh, P, Q = get_model()
-        bound = m.addConstr(chsh <= 2.5 + upper_bound)
+        game, chsh, P, Q = get_model(game)
+        bound = game.model.addConstr(chsh <= 2.5 + upper_bound)
 
-        m.update()
-        m.setObjective(chsh, GRB.MAXIMIZE)
-        m.update()
+        game.model.update()
+        game.model.setObjective(chsh, GRB.MAXIMIZE)
+        game.model.update()
 
-        m.optimize()
-        m.remove(bound)
+        game.model.optimize()
+        game.model.remove(bound)
         upper_bound += d
 
         chsh_check = 0
-        for x, y in product(domain_xy, repeat=2):
+        for x, y in product(game.domain_xy, repeat=2):
             e = 0
-            for a, b in product(domain_ab, repeat=2):
-                e += a * b * P[p[a, b, x, y]].X
+            for a, b in product(game.domain_ab, repeat=2):
+                e += a * b * P[game.indexes_p[a, b, x, y]].X
             chsh_check += (-1) ** (x * y) * asin(e)
 
         print(
@@ -141,14 +119,14 @@ def find_optimal_p():
         run = chsh.getValue() <= 2 * sqrt(2)
         # input()
 
-    print(f"Objective = {m.objVal}")
+    print(f"Objective = {game.model.objVal}")
     print(f"{Q.X = }")
     print(f"Computed probability distribution : \n{([P[i].X for i in range(16)])}")
     chsh_check = 0
-    for x, y in product(domain_xy, repeat=2):
+    for x, y in product(game.domain_xy, repeat=2):
         e = 0
-        for a, b in product(domain_ab, repeat=2):
-            e += a * b * P[p[a, b, x, y]].X
+        for a, b in product(game.domain_ab, repeat=2):
+            e += a * b * P[game.indexes_p[a, b, x, y]].X
         chsh_check += (-1) ** (x * y) * asin(e)
 
     print(
@@ -156,51 +134,8 @@ def find_optimal_p():
     )
 
 
-def using_bit_mask():
-    def ls_quantum_p():
-        """
-        Quantum probability distribution,
-        computed by solving the system of equations.
-        """
-        # Constraint Coefficients
-        A = np.zeros([16, 16])
-        # Left Side
-        B = np.zeros([16])
-        i = 0
-
-        # <ψ| A_x x B_y |ψ> = xy/sqrt(2)
-        for x, y in product(domain_xy, repeat=2):
-            for a, b in product(domain_ab, repeat=2):
-                A[i, p[a, b, x, y]] = a * b
-
-            B[i] = (-1) ** (x * y) / sqrt(2)
-            i += 1
-
-        # <ψ| A_x x B_y |ψ> = 1
-        for x, y in product(domain_xy, repeat=2):
-            for a, b in product(domain_ab, repeat=2):
-                A[i, p[a, b, x, y]] = 1
-            B[i] = 1
-            i += 1
-
-        # <ψ| A_x x 1_B |ψ> = 0
-        for x, y in product(domain_xy, repeat=2):
-            for a, b in product(domain_ab, repeat=2):
-                A[i, p[a, b, x, y]] = a
-            B[i] = 0
-            i += 1
-
-        # <ψ| 1_A x B_y |ψ> = 0
-        for x, y in product(domain_xy, repeat=2):
-            for a, b in product(domain_ab, repeat=2):
-                A[i, p[a, b, x, y]] = b
-            B[i] = 0
-            i += 1
-
-        # Solve linear system Ax = B
-        return list(np.linalg.solve(A, B))
-
-    use_qp_mask = [  # 0 and 4 to Tru gives a quantum behavior
+def using_bit_mask(game):
+    use_qp_mask = [
         True,
         False,
         False,
@@ -219,7 +154,7 @@ def using_bit_mask():
         False,
     ]
 
-    m, chsh, P, Q = get_model()
+    m, chsh, P, Q = get_model(game)
 
     previous_chsh = 0
     run = True
@@ -229,28 +164,28 @@ def using_bit_mask():
     c = Context(prec=15)
 
     while run:
-        m, chsh, P, Q = get_model()
-        qp = ls_quantum_p()
+        game, chsh, P, Q = get_model(game)
+        qp = quantum_probability_distribution_chsh(game)
 
         for i in range(16):
             if use_qp_mask[i]:
-                m.addConstr(P[i] == qp[i])
+                game.model.addConstr(P[i] == qp[i])
 
-        bound = m.addConstr(chsh >= 2 * sqrt(2) - upper_bound)
+        bound = game.model.addConstr(chsh >= 2 * sqrt(2) - upper_bound)
 
-        m.update()
-        m.setObjective(chsh, GRB.MINIMIZE)
-        m.update()
+        game.model.update()
+        game.model.setObjective(chsh, GRB.MINIMIZE)
+        game.model.update()
 
-        m.optimize()
-        m.remove(bound)
+        game.model.optimize()
+        game.model.remove(bound)
         upper_bound += d
 
         chsh_check = 0
-        for x, y in product(domain_xy, repeat=2):
+        for x, y in product(game.domain_xy, repeat=2):
             e = 0
-            for a, b in product(domain_ab, repeat=2):
-                e += a * b * P[p[a, b, x, y]].X
+            for a, b in product(game.domain_ab, repeat=2):
+                e += a * b * P[game.indexes_p[a, b, x, y]].X
             chsh_check += (-1) ** (x * y) * asin(e)
 
         print(
@@ -260,14 +195,14 @@ def using_bit_mask():
         run = chsh.getValue() >= 1 and previous_chsh != chsh.getValue()
         previous_chsh = chsh.getValue()
 
-    print(f"Objective = {m.objVal}")
+    print(f"Objective = {game.model.objVal}")
     print(f"{Q.X = }")
     print(f"Computed probability distribution : \n{([P[i].X for i in range(16)])}")
     chsh_check = 0
-    for x, y in product(domain_xy, repeat=2):
+    for x, y in product(game.domain_xy, repeat=2):
         e = 0
-        for a, b in product(domain_ab, repeat=2):
-            e += a * b * P[p[a, b, x, y]].X
+        for a, b in product(game.domain_ab, repeat=2):
+            e += a * b * P[game.indexes_p[a, b, x, y]].X
         chsh_check += (-1) ** (x * y) * asin(e)
 
     print(
@@ -275,4 +210,6 @@ def using_bit_mask():
     )
 
 
-using_bit_mask()
+if __name__ == "__main__":
+    chsh_game = Game(domain_ab=[-1, 1], domain_xy=[0, 1])
+    find_optimal_p(chsh_game)
